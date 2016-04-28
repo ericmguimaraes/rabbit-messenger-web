0c3603c18ec4dd93797c1fdabecd845427d2f261
ajalvesneto <antonio.janeto@gmail.com>
2016-04-28 16:01:30 -0300

    Primeiro commit

---
+++ src/web/Send.java
@@ -0,0 +1,30 @@
+package web;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class Send {
+
+  private final static String QUEUE_NAME = "hello";
+
+  public static void main(String[] argv) throws Exception {
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setHost("ajalvesneto.koding.io");
+    Connection connection = factory.newConnection();
+    Channel channel = connection.createChannel();
+
+    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+    String message = "Abacaxi!";
+    channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+    for (int i = 0; i < 10; i++) {
+    	channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+    	System.out.println(" [x] Sent '" + message + "'");
+	}
+		
+    
+    
+
+    channel.close();
+    connection.close();
+  }
+}
