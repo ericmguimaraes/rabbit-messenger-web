68ed75d16a4d945382bb4b2bd6db98aa5d5af63a
ajalvesneto <antonio.janeto@gmail.com>
2016-04-28 16:40:02 -0300

    Modificando o send pra receber entrada do teclado

--- src/web/Send.java
+++ src/web/Send.java
@@ -1,4 +1,6 @@
 package web;
+import java.util.Scanner;
+
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
@@ -8,23 +10,29 @@ public class Send {
   private final static String QUEUE_NAME = "hello";
 
   public static void main(String[] argv) throws Exception {
-    ConnectionFactory factory = new ConnectionFactory();
+    
+	 ConnectionFactory factory = new ConnectionFactory();
     factory.setHost("ajalvesneto.koding.io");
     Connection connection = factory.newConnection();
     Channel channel = connection.createChannel();
 
     channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-    String message = "Abacaxi!";
-    channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
-    for (int i = 0; i < 10; i++) {
+    String message = "";
+    
+    Scanner scanner = new Scanner(System.in);
+    
+    while (!message.equals("sair")){
+    	message = scanner.nextLine();
     	channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
-    	System.out.println(" [x] Sent '" + message + "'");
-	}
-		
+    }	
+    
+    channel.close();
+    connection.close();
+    
+    
     
     
 
-    channel.close();
-    connection.close();
+    
   }
 }
